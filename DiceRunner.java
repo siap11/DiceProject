eb65e6242139b25e5c3dcd4b0153b7995f97bdee
Sia Pyati <10016346@sbstudents.org>
2018-12-05 08:30:41 -0500

    Created Test 1 for runner
    Created and printed test 1 for runner

--- DiceRunner.java
+++ DiceRunner.java
@@ -1,5 +1,30 @@
 public class DiceRunner{
-public static void main(String[]args){
+	public static void main(String[]args){
 
-}
+	// Test 1
+		Die die = new Die();
+		Die die2 = new Die();
+
+		int rollCount = 0;
+		int sum = 0;
+
+		do{
+			die.roll();
+			die2.roll();
+			rollCount++;
+
+			System.out.println("Roll Number: " + rollCount);
+			System.out.println(die.toString());
+			System.out.println(die2.toString());
+			System.out.println();
+
+			sum = die.getValue() + die2.getValue();
+
+		}while(sum!=2);
+
+		System.out.println("It took " + rollCount + " rolls to get snake eyes.");
+
+
+
+	}
 }
