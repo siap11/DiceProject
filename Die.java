41d352a9863b30559969d9952167804ab2b2bc84
Sia Pyati <10016346@sbstudents.org>
2018-12-05 07:59:56 -0500

    Created die class
    wrote all code for die class

--- Die.java
+++ Die.java
@@ -1,3 +1,33 @@
 class Die{
 
+	private int sides;
+	private int value;
+
+	public Die(){
+		sides = 6;
+		value = (int)(Math.random()*6)+1;
+	}
+
+	public Die(int sides){
+		this.sides = sides;
+		value = (int)(Math.random()*sides)+1;
+	}
+
+	public int getValue(){
+		return this.value;
+	}
+
+	public int getSides(){
+		return this.sides;
+	}
+
+	public void roll(){
+		value = (int)(Math.random()*sides)+1;
+	}
+
+	public String toString(){
+		return "Number of sides:" + sides + ", Value:" + value;
+
+	}
+
 }
